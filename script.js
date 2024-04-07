07f6a065289f28a2c9686c9782e85f37b03b3297
sania924 <saniayasin924@gmail.com>
2024-04-08 00:33:52 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var page1Content = document.querySelector("#page1-content");
+var cursor = document.querySelector("#cursor");
+page1Content.addEventListener("mousemove", function (data) {
+  gsap.to(cursor, {
+    x: data.x,
+    y: data.y,
+  });
+});
+
+page1Content.addEventListener("mouseenter", function () {
+  gsap.to(cursor, {
+    scale: 1,
+    opacity: 1,
+  });
+});
+
+page1Content.addEventListener("mouseleave", function () {
+  gsap.to(cursor, {
+    scale: 0,
+    opacity: 0,
+  });
+});
