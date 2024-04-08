a4fd35b65843d9bbde5b3c5f64df087c2fba0ff8
sania924 <saniayasin924@gmail.com>
2024-04-08 22:01:39 +0500

    half complete

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-var page1Content = document.querySelector("#page1-content");
-var cursor = document.querySelector("#cursor");
-page1Content.addEventListener("mousemove", function (data) {
-  gsap.to(cursor, {
-    x: data.x,
-    y: data.y,
+function cursorEffect() {
+  var page1Content = document.querySelector("#page1-content");
+  var cursor = document.querySelector("#cursor");
+  page1Content.addEventListener("mousemove", function (data) {
+    gsap.to(cursor, {
+      x: data.x,
+      y: data.y,
+    });
   });
-});
 
-page1Content.addEventListener("mouseenter", function () {
-  gsap.to(cursor, {
-    scale: 1,
-    opacity: 1,
+  page1Content.addEventListener("mouseenter", function () {
+    gsap.to(cursor, {
+      scale: 1,
+      opacity: 1,
+    });
   });
-});
 
-page1Content.addEventListener("mouseleave", function () {
-  gsap.to(cursor, {
-    scale: 0,
-    opacity: 0,
+  page1Content.addEventListener("mouseleave", function () {
+    gsap.to(cursor, {
+      scale: 0,
+      opacity: 0,
+    });
   });
-});
+}
+cursorEffect();
